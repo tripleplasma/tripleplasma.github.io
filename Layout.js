7451c55df3ed6ad4df668022f02703c8c0c11a0d
Kung Lee <kung@hortonvillerobotics.com>
2020-09-30 16:39:45 -0500

    Initial Commit

---
+++ Layout.js
@@ -0,0 +1,17 @@
+const e = React.createElement;
+//please work
+class NavBar extends React.Component {
+  render() {
+    const names = ["Abby","Beau","Bianca","Cade","Ethan","Evan","Evanna","Grace","Kayla","Kung","Megan","Natalie","Tanner"];
+    const links = names.map(item =>
+      e("li",{className:"nav-item"},e("a",{className:"nav-link",href:"./"+item+".html"},item))
+    );
+    return e(
+      'nav',
+      {className:"navbar navbar-expand-sm bg-dark navbar-dark"},
+      [e('a',{className:"navbar-brand",href:"./index.html"},"Happy Tree Friends Blog"),
+       e('ul',{className:"navbar-nav"},
+       links)]);
+  }
+}
+ReactDOM.render(e(NavBar), document.getElementById('navplace'));
