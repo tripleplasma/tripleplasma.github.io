2d3498d588bdc7442f9d53fd28b85fba0be51682
Kung Lee <kung@hortonvillerobotics.com>
2020-09-30 21:39:53 -0500

    pathing

--- Layout.js
+++ Layout.js
@@ -3,12 +3,12 @@ class NavBar extends React.Component {
   render() {
     const names = ["Abby","Beau","Bianca","Cade","Ethan","Evan","Evanna","Grace","Kayla","Kung","Megan","Natalie","Tanner"];
     const links = names.map(item =>
-      e("li",{className:"nav-item"},e("a",{className:"nav-link",href:item+".html"},item))
+      e("li",{className:"nav-item"},e("a",{className:"nav-link",href:"/"+item+"/"+item+".html"},item))
     );
     return e(
       'nav',
       {className:"navbar navbar-expand-sm bg-dark navbar-dark"},
-      [e('a',{className:"navbar-brand",href:"./index.html"},"Happy Tree Friends Blog"),
+      [e('a',{className:"navbar-brand",href:"/index.html"},"Happy Tree Friends Blog"),
        e('ul',{className:"navbar-nav"},
        links)]);
   }
