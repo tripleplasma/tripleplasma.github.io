b4039a3745a26e726400b23f7848b83254e32497
Kung Lee <kung@hortonvillerobotics.com>
2020-10-03 21:48:40 -0500

    side bar and home pages

--- indexscripts.js
+++ indexscripts.js
@@ -1,7 +1,6 @@
 class GroupEvents extends React.Component {
   render() {
-    //const pics = 
     return e("h1",null,"Test");
   }
 }
-ReactDOM.render(e(GroupEvents), document.getElementById('group-events'));
+ReactDOM.render(e(GroupEvents), document.getElementById("test"));
