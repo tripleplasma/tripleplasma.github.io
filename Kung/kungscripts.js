ac19a681a41b1092e84a0bd79e70cc82d0520b80
Kung Lee <kung@hortonvillerobotics.com>
2020-10-12 15:33:18 -0500

    tried to add react to kung page

--- Kung/kungscripts.js
+++ Kung/kungscripts.js
@@ -7,9 +7,9 @@ $(window).scroll(function(){
     $("#sidenav").css("top",pix+"px");
   }
 });
-
 var slideIndex = 1;
 showSlides(slideIndex);
+
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -34,3 +34,38 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// class KungShow extends React.Component{
+//   render(){
+//     class Picture{
+//       constructor(picName,caption){
+//         this.url = "kungpics/"+picName+".jpg";
+//         this.caption = caption;
+//       }
+//     }
+//     const pics = [
+//       new Picture("jacket_beanie","Kung in a Jacket"),
+//       new Picture("pose","Cool Pose"),
+//       new Picture("stare","Kung with red glasses"),
+//       new Picture("looking","Kung with red glasses #2"),
+//       new Picture("skaterboy","Skater boy Kung"),
+//       new Picture("winterKung","Winter Kung")];
+//
+//     var slides = pics.map(pic =>
+//       e("div",{className:"slide"},[
+//         e("img",{src:pic.url},null),
+//         e("div",{className:"slide-caption"},pic.caption)
+//       ])
+//     );
+//     slides.push(e("a",{className:"prev"},"<"));
+//     slides.push(e("a",{className:"next"},">"));
+//
+//     var dots = [];
+//     for(var i = 0; i < pics.length; i++){
+//       dots.push(e("span",{className:"dot"},null));
+//     }
+//     slides.push(e("div",{style:"text-align:center"},dots));
+//     return slides;
+//   }
+// }
+// ReactDOM.render(e(KungShow),document.getElementById("slide-show1"));
