9f091634a6a6ddefe8e4f1dc9b66ee387158308d
Kung Lee <kung@hortonvillerobotics.com>
2020-10-12 17:08:53 -0500

    finished adding react ot kung page

--- Kung/kungscripts.js
+++ Kung/kungscripts.js
@@ -7,65 +7,81 @@ $(window).scroll(function(){
     $("#sidenav").css("top",pix+"px");
   }
 });
-var slideIndex = 1;
-showSlides(slideIndex);
 
+var currentSlide = 0;
+$(document).ready(function(){
+  $(".prev").attr("onclick","plusSlides(-1)");
+  $(".next").attr("onclick","plusSlides(1)");
+  var dots = $(".dot");
+  for(var i = 0; i < dots.length; i++){
+    $(dots[i]).attr("onclick","toCurrentSlide("+i+")");
+  }
+  showSlides(0);
+});
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  currentSlide += n;
+  showSlides(currentSlide);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function toCurrentSlide(n) {
+  currentSlide = n;
+  showSlides(n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = $(".slide");
+
   var dots = $(".dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+
+  if(n >= $(".slide").length+1){currentSlide = 0;}
+  else if(n < 0){currentSlide = $(".slide").length;}
+
+  var appearSlides = $(".slide-appear");
+
+  for (var i = 0; i < appearSlides.length; i++) {
+    appearSlides[i].className = "slide";
   }
-  for (i = 0; i < dots.length; i++) {
+
+  for (var i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  var slides = $(".slide");
+  slides[currentSlide].className = "slide-appear";
+  dots[currentSlide].className += " active";
 }
 
-// class KungShow extends React.Component{
-//   render(){
-//     class Picture{
-//       constructor(picName,caption){
-//         this.url = "kungpics/"+picName+".jpg";
-//         this.caption = caption;
-//       }
-//     }
-//     const pics = [
-//       new Picture("jacket_beanie","Kung in a Jacket"),
-//       new Picture("pose","Cool Pose"),
-//       new Picture("stare","Kung with red glasses"),
-//       new Picture("looking","Kung with red glasses #2"),
-//       new Picture("skaterboy","Skater boy Kung"),
-//       new Picture("winterKung","Winter Kung")];
-//
-//     var slides = pics.map(pic =>
-//       e("div",{className:"slide"},[
-//         e("img",{src:pic.url},null),
-//         e("div",{className:"slide-caption"},pic.caption)
-//       ])
-//     );
-//     slides.push(e("a",{className:"prev"},"<"));
-//     slides.push(e("a",{className:"next"},">"));
-//
-//     var dots = [];
-//     for(var i = 0; i < pics.length; i++){
-//       dots.push(e("span",{className:"dot"},null));
-//     }
-//     slides.push(e("div",{style:"text-align:center"},dots));
-//     return slides;
-//   }
-// }
-// ReactDOM.render(e(KungShow),document.getElementById("slide-show1"));
+class KungShow extends React.Component{
+  render(){
+    class Picture{
+      constructor(picName,caption){
+        this.url = "kungpics/"+picName+".jpg";
+        this.caption = caption;
+      }
+    }
+    const pics = [
+      new Picture("jacket_beanie","Kung in a Jacket"),
+      new Picture("pose","Cool Pose"),
+      new Picture("stare","Kung with red glasses"),
+      new Picture("looking","Kung with red glasses #2"),
+      new Picture("skaterboy","Skater boy Kung"),
+      new Picture("winterKung","Winter Kung")];
+
+    var slides = pics.map(pic =>
+      e("div",{className:"slide"},[
+        e("img",{src:pic.url},null),
+        e("div",{className:"slide-caption"},pic.caption)
+      ])
+    );
+    slides.push(e("a",{className:"prev"},"\u27E8"));
+    slides.push(e("a",{className:"next"},"\u27E9"));
+
+    var dots = [];
+    for(var i = 0; i < pics.length; i++){
+      dots.push(e("span",{className:"dot"},null));
+    }
+    slides.push(e("div",{className:"dotDiv"},dots));
+    return slides;
+  }
+}
+ReactDOM.render(e(KungShow),document.getElementById("slide-show1"));
