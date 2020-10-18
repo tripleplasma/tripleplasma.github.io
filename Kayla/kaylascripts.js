e3237f2e6536e6ecbe31c12a24d8cbaa61cde073
Kung Lee <kung@hortonvillerobotics.com>
2020-10-17 19:02:26 -0500

    updated kayla's page

---
+++ Kayla/kaylascripts.js
@@ -0,0 +1,76 @@
+var currentSlide = 0;
+$(document).ready(function(){
+  $(".prev").attr("onclick","plusSlides(-1)");
+  $(".next").attr("onclick","plusSlides(1)");
+  var dots = $(".dot");
+  for(var i = 0; i < dots.length; i++){
+    $(dots[i]).attr("onclick","toCurrentSlide("+i+")");
+  }
+  showSlides(0);
+});
+
+function plusSlides(n) {
+  currentSlide += n;
+  showSlides(currentSlide);
+}
+
+function toCurrentSlide(n) {
+  currentSlide = n;
+  showSlides(n);
+}
+
+function showSlides(n) {
+
+  var dots = $(".dot");
+
+  if(n >= $(".slide").length+1){currentSlide = 0;}
+  else if(n < 0){currentSlide = $(".slide").length;}
+
+  var appearSlides = $(".slide-appear");
+
+  for (var i = 0; i < appearSlides.length; i++) {
+    appearSlides[i].className = "slide";
+  }
+
+  for (var i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  var slides = $(".slide");
+  slides[currentSlide].className = "slide-appear";
+  dots[currentSlide].className += " active";
+}
+
+class KaylaShow extends React.Component{
+  render(){
+    class Picture{
+      constructor(picName,caption){
+        this.url = "kaylapics/"+picName+".jpg";
+        this.caption = caption;
+      }
+    }
+    const pics = [
+      new Picture("Kayla","Me"),
+      new Picture("halloweenkayla","It's Almost Halloween"),
+      new Picture("lockedinhousekayla","Nothing Better To Do"),
+      new Picture("skaterkayla","Skater Girl")
+    ];
+
+    var slides = pics.map(pic =>
+      e("div",{className:"slide"},[
+        e("img",{src:pic.url},null),
+        e("div",{className:"slide-caption"},pic.caption)
+      ])
+    );
+    slides.push(e("a",{className:"prev"},"\u27E8"));
+    slides.push(e("a",{className:"next"},"\u27E9"));
+
+    var dots = [];
+    for(var i = 0; i < pics.length; i++){
+      dots.push(e("span",{className:"dot"},null));
+    }
+    slides.push(e("div",{className:"dotDiv"},dots));
+    return slides;
+  }
+}
+ReactDOM.render(e(KaylaShow),document.getElementById("slide-show1"));
